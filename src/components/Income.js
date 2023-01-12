23fa52d8901173759a15377bc643a8ab3735794b
filipjovicccc <filipjovicccc@gmail.com>
2023-01-13 00:50:44 +0100

    working on income-table

--- src/components/Income.js
+++ src/components/Income.js
@@ -4,53 +4,66 @@ import image2 from "../assests/img/bank2.png"
 
 const Income =() =>{
   const{items} = useContext(incomeContext)
-  console.log(items)
+  console.log(items.map(item => item.text)[0])
   
 
     return(
        
   <div className="flex opacity-85 items-center justify-center h-screen h-screen bg-center bg-cover" style={{backgroundImage: `url(${image2})`}}>
    
-    <div className="w-6/12 h-1/3 bg-green-500
+    <div className="w-7/12 h-1/2 bg-green-500
      p-4 rounded-lg shadow-lg flex flex-col" >
+     {items.map(item => {
+        return(
+     <div className="w-28 h-16 text-white text-2xl  flex justify-center items-center bg-red-500 mt-20">${(item.text[0])}</div>
+
+        )
+     })}
         <table className="table-auto w-full m-auto">
             <thead className="bg-gray-200">
                 <tr>
-                    <th className="px-4 bg-red-500 py-2 text-white font-medium">Header 1</th>
-                    <th className="px-4 bg-red-500 py-2 text-white font-medium">Header 2</th>
-                    <th className="px-4 bg-red-500 py-2 text-white font-medium">Header 3</th>
-                    <th className="px-4 bg-red-500 py-2 text-white font-medium">Header 4</th>
-                    <th className="px-4 bg-red-500 py-2 text-white font-medium">Header 5</th>
+                    <th className="px-4 text-left bg-red-500 py-2 text-white font-medium">Frequency</th>
+                    <th className="px-4 text-left  bg-red-500 py-2 text-white font-medium">Gross income</th>
+                    <th className="px-4 text-left  bg-red-500 py-2 text-white font-medium">Tax</th>
+                    <th className="px-4 text-left  bg-red-500 py-2 text-white font-medium">Net income</th>
+                   
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td className="border px-4 py-2">Data 1</td>
-                    <td className="border px-4 py-2">Data 2</td>
-                    <td className="border px-4 py-2">Data 3</td>
-                    <td className="border px-4 py-2">Data 4</td>
-                    <td className="border px-4 py-2">Data 5</td>
+                    <td className="border px-4 py-2">Weekly</td>
+                    <td className="border px-4 py-2">Fortnightly</td>
+                    <td className="border px-4 py-2">Monthly</td>
+                    <td className="border px-4 py-2">Annually</td>
+                 
                 </tr>
                 <tr className="bg-gray-100">
                     <td className="border px-4 py-2">Data 1</td>
                     <td className="border px-4 py-2">Data 2</td>
                     <td className="border px-4 py-2">Data 3</td>
                     <td className="border px-4 py-2">Data 4</td>
-                    <td className="border px-4 py-2">Data 5</td>
+                  
                 </tr>
                 <tr>
                     <td className="border px-4 py-2">Data 1</td>
                     <td className="border px-4 py-2">Data 2</td>
                     <td className="border px-4 py-2">Data 3</td>
                     <td className="border px-4 py-2">Data 4</td>
-                    <td className="border px-4 py-2">Data 5</td>
+                  
                 </tr>
                 <tr className="bg-gray-100">
                     <td className="border px-4 py-2">Data 1</td>
                     <td className="border px-4 py-2">Data 2</td>
                     <td className="border px-4 py-2">Data 3</td>
                     <td className="border px-4 py-2">Data 4</td>
-                    <td className="border px-4 py-2">Data 5</td>
+                   
+                </tr>
+                <tr >
+                    <td className="border px-4 py-2">Data 1</td>
+                    <td className="border px-4 py-2">Data 2</td>
+                    <td className="border px-4 py-2">Data 3</td>
+                    <td className="border px-4 py-2">Data 4</td>
+                   
                 </tr>
               </tbody>
             </table>
--- src/components/IncomeForm.js
+++ src/components/IncomeForm.js
@@ -26,8 +26,7 @@ const IncomeForm = (props) => {
      
 
           <div className="w-7/12 h-1/2 bg-red-500 p-10 absolute p-4 z-index:10 rounded-lg shadow-lg flex flex-col">
-          {/* <Link className="bg-blue-500 text-white py-1 px-1 rounded-lg mr-2" to="income">Income</Link> */}
-        <h1 className="text-2xl font-medium text-center">Income Tax Calculator</h1>
+         <h1 className="text-2xl font-medium text-center">Income Tax Calculator</h1>
         <form className="mt-4 divide-y divide-gray-400">
             <div className="py-2">
                 <label className="block text-gray-700 font-medium">What is your total income?</label>
@@ -44,12 +43,12 @@ const IncomeForm = (props) => {
             <div className="py-2">
                 <h1 className="text-gray-700 font-medium">Please choose the income type</h1>
                 <div className="flex items-center mt-5 justify-center">
-                    <button className="bg-green-500 text-white py-2 px-4 rounded-lg mr-2">Gross Income</button>
+                    <button onClick={submitHandler}  className="bg-green-500 text-white py-2 px-4 rounded-lg mr-2">Gross Income</button>
                     <button className="bg-green-500 text-white py-2 px-4 rounded-lg">Net Income</button>
                 </div>
             </div>
             <div className="py-2 flex items-center">
-                <button onClick={submitHandler} className="bg-blue-500 w-3/5 text-white mt-10  py-2 px-8 rounded-lg m-auto">Calculate</button>
+              <Link to="income" className="bg-blue-500 w-3/5 text-white mt-10  py-2 px-8 rounded-lg m-auto">Calculate</Link>  
             </div>
         </form>
             </div>
